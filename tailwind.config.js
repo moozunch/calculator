b500920653addf0f226a577abc39996332de5042
annisa :b <112236945+moozunch@users.noreply.github.com>
2024-10-15 17:51:38 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,6 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
-      fontSize: {
-        'custom-1.5rem': '1.5rem',
-      },
-      colors: {
-        'black': '#313131',
-      },
-      cursor: {
-        'custom-pointer': 'pointer',
-      },
-      fontFamily: {
-        'arial': ['Arial', 'sans-serif'],
-      },
     },
   },
   plugins: [],
